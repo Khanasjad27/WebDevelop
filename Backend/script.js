40afb815f106bce663cd0eb2f8a0e7c0fc9b105a
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2025-09-02 02:17:18 +0530

    learn all about module exports

--- Backend/script.js
+++ Backend/script.js
@@ -1,8 +1,8 @@
-// let n = 1000;
+let n = 10;
 
-// for(let i=0;i<n;i++){
-//     console.log("Hello",i);
-// }
+for(let i=0;i<n;i++){
+    console.log("Hello",i);
+}
 
 
 let arg = process.argv;
@@ -10,3 +10,4 @@ let arg = process.argv;
 for(let i=2;i<arg.length;i++){
     console.log("Hello to", arg[i]);
 }
+
