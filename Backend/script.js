00505108a54869298893912e6dd2524e9c8f3333
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2025-09-02 01:39:02 +0530

    Implementation of process.argvv
    Previously we had undestand how to run vs code ka code in MINGW terminal n now we used argv.

--- Backend/script.js
+++ Backend/script.js
@@ -1,5 +1,12 @@
-let n = 1000;
+// let n = 1000;
 
-for(let i=0;i<n;i++){
-    console.log("Hello",i);
+// for(let i=0;i<n;i++){
+//     console.log("Hello",i);
+// }
+
+
+let arg = process.argv;
+
+for(let i=2;i<arg.length;i++){
+    console.log("Hello to", arg[i]);
 }
