723fed82050beb73b532c30b2572562cc1ca550b
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2025-09-03 01:20:34 +0530

    Add EJS views and routing for home, dice, and Instagram
    Configured Express to use EJS templates and added three views: home.ejs, rolldice.ejs, and Insta.ejs. Implemented routes for the home page, a dice roll page with random value, and an Instagram-like user page displaying followers.

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 let port = 3000;
+// define path if trying to run from parent folder
+app.set("views",path.join(__dirname, "/views"));
 
+app.set("view engine","ejs");
+
+
+// created a server
 app.listen(port,()=>{
     console.log(`listning on port ${port}`);
 });
+
+
+// for home page
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
+// for dice
+app.get("/dice",(req,res)=>{
+    let random = Math.floor(Math.random() * 6) + 1
+    res.render("rolldice.ejs",{random});
+});
+
+// for insta landing page jo maine banaya hai
+app.get("/ig/:username" , (req, res)=>{
+    let follower = ["alex" , "streave", "job"];
+    let { username } = req.params;
+    res.render("Insta.ejs",{ username , follower });
+});
+
+
+
