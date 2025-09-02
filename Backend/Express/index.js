7a169fcafb5cfc9600aa1f8269ad2906518be56f
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2025-09-02 15:15:08 +0530

    Move figlet module to new directory structure
    Relocated the figlet module and all its files from 'Backend/figlet' to 'Backend/1st section/figlet' to improve project organization and maintain consistency in the backend directory structure.

---
+++ Backend/Express/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+app.listen(port, ()=>{
+    console.log(`app is lisning ${port}`);
+});
+
+app.use((req,res) => {
+    console.log("Request Recived");
+});
