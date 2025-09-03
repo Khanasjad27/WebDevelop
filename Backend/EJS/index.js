0c10e731af498d235aa6a07f6aa0dfdadce7b5e2
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2025-09-03 12:57:59 +0530

    Add Instagram profile rendering with data.json
    Implemented dynamic Instagram profile pages using EJS templates and data from data.json. Added static assets (CSS and JS), updated routing logic in index.js to render profiles or error page, and improved Insta.ejs to display user data and posts.

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+let port = 8080;
 
-let port = 3000;
-// define path if trying to run from parent folder
+// to connect public folder
+// app.use(express.static("public"));
+
+// define path if trying to run from parent folder for views
+app.use(express.static(path.join(__dirname, "/public/js")));
+app.use(express.static(path.join(__dirname, "/public/css")));
+// define path if trying to run from parent folder for views
 app.set("views",path.join(__dirname, "/views"));
 
 app.set("view engine","ejs");
@@ -26,10 +32,26 @@ app.get("/dice",(req,res)=>{
 });
 
 // for insta landing page jo maine banaya hai
-app.get("/ig/:username" , (req, res)=>{
-    let follower = ["alex" , "streave", "job"];
+// app.get("/ig/:username" , (req, res)=>{
+//     let follower = ["alex" , "streave", "job"];
+//     let { username } = req.params;
+//     res.render("Insta.ejs",{ username , follower });
+// });
+
+
+// insta Landing page if we get data from data base
+// we had copy code this is given in cource n created a file called as data.json
+
+app.get("/ig/:username", (req,res) =>{
     let { username } = req.params;
-    res.render("Insta.ejs",{ username , follower });
+    const instaData = require("./data.json");
+    const data = instaData[username]; 
+    
+    if(data){
+        res.render("insta.ejs", { data });
+    }else{
+        res.render("error.ejs");
+    } 
 });
 
 
