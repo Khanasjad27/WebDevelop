50f5fd073af08170dc7d66bfb1cccf9d5c9d214f
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2025-09-22 21:38:07 +0530

    Add and update npm dependencies
    Added multiple new packages to node_modules including express, body-parser, async, ejs, and others. Updated package-lock.json to reflect new and updated dependencies for the Backend/SQL_DATA project.

--- Backend/SQL_DATA/index.js
+++ Backend/SQL_DATA/index.js
@@ -1,9 +1,18 @@
+// requireing express n ejs
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+
 // Connection building
 // Get the client
-import mysql from 'mysql2/promise';
+const mysql = require("mysql2");
 
 // Create the connection to database
-const connection = await mysql.createConnection({
+const connection =  mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'delta_app',
@@ -13,22 +22,71 @@ const connection = await mysql.createConnection({
 const { faker } = require('@faker-js/faker');
 
 let   getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
 };
 
 
-try{
-    connection.query("SHOW TABLE", (err,result)=>{
+// humne jo upar connection banaya hai usme ek query naaam ka fu hai usko use karre hai
+// try{
+//     connection.query("SHOW TABLEs", (err,result)=>{
+//         if(err) throw err;
+//         console.log(result);
+//     });
+// }catch(err){
+//     console.log(err);
+// };
+
+//INSERT USER
+// let q = "INSERT INTO user (id, name, email, password) VALUES ?";
+// let data = [];
+// for(let i=1;i<=100;i++){
+//   data.push(getRandomUser());
+// }
+
+// try{
+//     connection.query(q,[data], (err,result)=>{
+//         if(err) throw err;
+//         console.log(result);
+//     });
+// }catch(err){
+//     console.log(err);
+// };
+// connection.end();
+
+// root 
+app.get("/" ,(req,res) =>{
+  q = "SELECT count(*) FROM user";
+  try{
+    connection.query(q, (err,result)=>{
         if(err) throw err;
-        console.log(result);
+        let count = result[0]["count(*)"];
+        res.render("home.ejs" , {count});
     });
-}catch(err){
+  }catch(err){
     console.log(err);
-};
+    res.send("Random error happen");
+  };
+});
 
-console.log(getRandomUser());
+// FOR ALL DATAS
+app.get("/user", (req,res) =>{
+  q = "SELECT * FROM user";
+  try{
+    connection.query(q, (err,users)=>{
+        if(err) throw err;
+        res.render("user.ejs", { users });
+    });
+  }catch(err){
+    console.log(err);
+    res.send("Random error happen");
+  };
+});
+
+app.listen("8080" , () =>{
+  console.log("Server is Started");
+});
